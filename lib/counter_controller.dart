28aa57dba9ec43e7920fb1586f584b1cedf33b2b
AndreasDevanPanduNarasamdya <andreasdevan123@gmail.com>
2026-02-11 20:21:12 +0700

    Modifikasi menurut Task 1: tombol kurang, reset, list

--- lib/counter_controller.dart
+++ lib/counter_controller.dart
@@ -1,12 +1,40 @@
+import 'package:flutter/material.dart';
+
 class CounterController {
   int _counter = 0; // Variabel private (Enkapsulasi)
-
+  int _step = 0;
   int get value => _counter; // Getter untuk akses data
+  List<List<String>> tracker_list = [];
+  List<String> tracked_list = [];
 
-  void increment() => _counter++;
-  void decrement() {
-    if (_counter > 0) _counter--;
+  void increment() {
+    _counter += _step;
+    update_list("Increment");
   }
 
-  void reset() => _counter = 0;
+  void decrement() {
+    if (_counter > 0) _counter -= _step;
+    update_list("Decrement");
+  }
+
+  void reset() {
+    _counter = 0;
+    update_list("Reset");
+  }
+
+  void setStep(int step) {
+    if (step > 0) {
+      _step = step;
+    }
+  }
+
+  void update_list(String action) {
+    final time = DateTime.now().toLocal().toString().substring(11, 19);
+    String logMessage = "$action at $time";
+    tracked_list.add(logMessage);
+    if (action == "Reset") {
+      tracker_list.add(List.from(tracked_list));
+      tracked_list.clear();
+    }
+  }
 }
--- lib/counter_view.dart
+++ lib/counter_view.dart
@@ -20,12 +20,46 @@ class _CounterViewState extends State<CounterView> {
           children: [
             const Text("Total Hitungan:"),
             Text('${_controller.value}', style: const TextStyle(fontSize: 40)),
+            TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              onChanged: (value) {
+                final step = int.tryParse(value);
+                if (step != null) {
+                  setState(() {
+                    _controller.setStep(step);
+                  });
+                }
+              },
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _controller.tracked_list.length,
+
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text(_controller.tracked_list[index]));
+                },
+              ),
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() => _controller.increment()),
-        child: const Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: () => setState(() => _controller.increment()),
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: () => setState(() => _controller.decrement()),
+            child: const Icon(Icons.minimize),
+          ),
+          FloatingActionButton(
+            onPressed: () => setState(() => _controller.reset()),
+            child: const Icon(Icons.restart_alt),
+          ),
+        ],
       ),
     );
   }
